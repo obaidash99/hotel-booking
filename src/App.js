e715889f39117c1e69592e1cbcb6d9be1fe71d3a
obaida <obaidashurbaji99@gmail.com>
2023-02-08 19:21:52 +0200

    add pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Bookings from './pages/Bookings';
 
 function App() {
 	return (
 		<div>
 			<Router>
 				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="contact" element={<Contact />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/bookings" element={<Bookings />} />
+				</Routes>
 			</Router>
 		</div>
 	);
