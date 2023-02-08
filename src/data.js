98b36d0be5b74dd3160778b58ad8df89ff90c36d
obaida <obaidashurbaji99@gmail.com>
2023-02-08 19:59:28 +0200

    home component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+	{
+		id: 1,
+		img: 'https://img.freepik.com/free-vector/male-senior-cartoon-character-carry-shopping-bag_1308-119278.jpg?w=2000',
+		title: 'summer sale',
+		desc: 'Summer Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur consectetur reprehenderit recusandae est impedit!',
+		bg: 'f5fafd',
+	},
+	{
+		id: 2,
+		img: 'https://png.pngtree.com/element_our/20190529/ourmid/pngtree-happy-shopping-boy-image_1203827.jpg',
+		title: 'winter sale',
+		desc: 'Winter Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur consectetur reprehenderit recusandae est impedit!',
+		bg: 'fbf0f4',
+	},
+	{
+		id: 3,
+		img: 'https://png.pngtree.com/element_our/20190529/ourmid/pngtree-happy-shopping-for-men-and-women-image_1203561.jpg',
+		title: 'autumn collection',
+		desc: 'Autumn Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut consequuntur consectetur reprehenderit recusandae est impedit!',
+		bg: 'fcf1ed',
+	},
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,115 @@
 import React from 'react';
+import Slider from '../components/Slider';
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="home">
+			<div className="slider-cover">
+				<Slider />
+			</div>
+			<div className="hotel-cards">
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+				<div className="card">
+					<img src="img-url" alt="hotel-name" />
+					<h2>Hotel Name</h2>
+					<p>Address</p>
+					<p>
+						Rating: <span>4.5/5</span>
+					</p>
+					<p>
+						Price per night: <span>$150</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
