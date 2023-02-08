45b853b78b1c73cddc00f4f80d77262a3b58f5e7
obaida <obaidashurbaji99@gmail.com>
2023-02-08 16:19:32 +0200

    navbar start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<nav className="navbar">
+			<div className="navbar-logo">
+				<Link to="/">Bookings</Link>
+			</div>
+			<div className="navbar-menu">
+				<ul className={`navbar-links ${isOpen ? 'show' : ''}`}>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact Us</Link>
+					</li>
+					<li>
+						<Link to="/about">About Us</Link>
+					</li>
+					<li>
+						<Link to="/bookings">My Bookings</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-toggle">
+				<button onClick={() => setIsOpen(!isOpen)}>
+					<MenuIcon />
+				</button>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
